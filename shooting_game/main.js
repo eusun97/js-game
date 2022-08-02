2c018ca13c98341cfb6d7f02da159bd50c46aa5e
Yueseon Lee <dksshd44@gmail.com>
2022-08-02 18:35:47 +0900

    roket 위치 선정/이벤트 처리 전

--- shooting_game/main.js
+++ shooting_game/main.js
@@ -13,6 +13,11 @@ document.body.appendChild(canvas) // body에 canvas 붙이기
 
 // 이미지 가져오기
 let backgroundImage, bulletImage, enemyImage, gameoverImage, roketImage;
+
+// 로켓 좌표 (계속 바뀌기때문에 따로 빼줌)
+let roketX = canvas.width / 2 - 30 // 로켓 픽셀 사이즈가 60*60
+let roketY = canvas.height - 60
+
 function loadImage() {
     backgroundImage = new Image();
     backgroundImage.src="images/background.png";
@@ -33,6 +38,7 @@ function loadImage() {
 // 이미지 보여주기 (image, x, y, dWidth, dHeight)
 function render() {
     ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+    ctx.drawImage(roketImage, roketX, roketY);
 }
 
 // 함수 호출
